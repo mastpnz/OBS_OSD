42ff785affa71f5d9c5261d3e3dc66fc0b94c8fd
Максим Степанцов <m.stepantsov@securitycode.ru>
2022-04-27 16:16:10 +0300

    new func parse

--- script_1.py
+++ script_1.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from docx.api import Document
 
 f = open(sys.argv[1], 'rb')
@@ -28,29 +29,37 @@ data = []
 #    row_data = dict(zip(keys, text))
 #    data.append(row_data)
 
+
 fo = open('squad.json', 'w')
 fo.write('{\n')
-fo.write('\"Players\" : [\n')
+teamparse = re.search(r'^Заявочный лист (.+), (.+)', document.paragraphs[0].text)
+fo.write('\t\"Name\" : \"')
+fo.write(teamparse.group(1))
+fo.write('\",\n')
+fo.write('\t\"City\" : \"')
+fo.write(teamparse.group(2))
+fo.write('\",\n')
+fo.write('\t\"Players\" : [\n')
 for i, row in enumerate(table.rows):
     if i > 0:
         if i > 1:
             fo.write(',\n')
-        str = '{ \"number\" : \"' + row.cells[0].text + '\", ' + '\"FIO\" : \"' + row.cells[1].text + '\", ' + '\"amplua\" : \"' + row.cells[2].text + '\", ' + '\"bdate\" : \"' + row.cells[4].text + '\" }'  
+        str = '\t\t{ "number\" : \"' + row.cells[0].text + '\", ' + '\"FIO\" : \"' + row.cells[1].text + '\", ' + '\"amplua\" : \"' + row.cells[2].text + '\", ' + '\"bdate\" : \"' + row.cells[4].text + '\" }'  
         fo.write(str)
 fo.write('\n')
-fo.write(']\n')
+fo.write('\t]')
 
 table = document.tables[1]
 fo.write(',\n')    
-fo.write('\"Stuff\" : [\n')
+fo.write('\t\"Stuff\" : [\n')
 for i, row in enumerate(table.rows):
     if i > 0:
         if i > 1:
             fo.write(',\n')
-        str = '{ \"amplua\" : ' + row.cells[2].text + '\", ' + '\"FIO\" : \"' + row.cells[1].text + '\", ' + '\"telnumb\" : \"' + row.cells[3].text + '\", ' + '\"email\" : \"' + row.cells[4].text + '\" }'
+        str = '\t\t{ \"amplua\" : ' + row.cells[2].text + '\", ' + '\"FIO\" : \"' + row.cells[1].text + '\", ' + '\"telnumb\" : \"' + row.cells[3].text + '\", ' + '\"email\" : \"' + row.cells[4].text + '\" }'
         fo.write(str)
 fo.write('\n')
-fo.write(']\n')
+fo.write('\t]\n')
 fo.write('}\n')
 fo.close()
 
